31ebac4743046c2ed7056c144a880cfb27ea285f
Onur Eker <yonureker@gmail.com>
2017-12-13 01:06:16 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,3 +30,11 @@ end
   def move(index,char = "X")
     @board[index] = char
   end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
