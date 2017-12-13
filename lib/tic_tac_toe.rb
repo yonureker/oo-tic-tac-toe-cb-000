de9cbe97b68a846e029515f50ef53a6b807b64df
Onur Eker <yonureker@gmail.com>
2017-12-13 01:17:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,4 +42,23 @@ end
   def turn_count
     @board.count{|token| token == "X" || token == "O"}
   end
+
+  def won?
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = @board[win_index_1] # load the value of the board at win_index_1
+    position_2 = @board[win_index_2] # load the value of the board at win_index_2
+    position_3 = @board[win_index_3] # load the value of the board at win_index_3
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+
+  end
+    else
+      false
+  end
 end
