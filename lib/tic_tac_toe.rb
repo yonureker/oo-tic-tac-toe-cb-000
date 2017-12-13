2326dacdd939268cac6e5d4e8eb48b1fb1364047
Onur Eker <yonureker@gmail.com>
2017-12-13 00:59:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,3 +22,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(index,char = "X")
+  @board[index] = char
+end
