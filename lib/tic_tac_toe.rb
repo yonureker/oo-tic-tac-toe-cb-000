4ca2dd471b24b832806fd73719df178e3d71178f
Onur Eker <yonureker@gmail.com>
2017-12-13 01:03:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,13 +15,13 @@ class TicTacToe
 ]
 end
 
- def display_board(board)
-   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-   puts "-----------"
-   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-   puts "-----------"
-   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
- end
+  def display_board(board)
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
 
   def input_to_index(user_input)
     user_input.to_i - 1
